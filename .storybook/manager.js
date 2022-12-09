93206202f69672b036c6b5fdbbef89cd9640ec0a
suman <ronaldosuman97@gmail.com>
2022-12-09 19:41:45 +0530

    feat: slick favicon add

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,5 +1,11 @@
 import { addons } from "@storybook/addons";
 import defaultTheme from "./theme";
+import favicon from "./public/storybook.png";
+
+const link = document.createElement("link");
+link.setAttribute("rel", "shortcut icon");
+link.setAttribute("href", favicon);
+document.head.appendChild(link);
 
 addons.setConfig({
   theme: defaultTheme,
--- .storybook/theme.js
+++ .storybook/theme.js
@@ -9,7 +9,7 @@ export default create({
   colorSecondary: "#603FEF",
 
   //   brandTitle: "Slick",
-  brandUrl: "https://example.com",
+  brandUrl: "/",
   brandImage:
     "https://res.cloudinary.com/dg4rjg58p/image/upload/c_fill,q_100,w_100/v1670489648/slick/Group_1512_ie4czu.png",
   brandTarget: "_self",
